3f8d5fdeb5ed7f9eef748160eb49d478190f4456
Mateusz Okon <mateusz.okon04@gmail.com>
2026-08-25 16:07:49 +0200

    feat: add signup and login endpoints

---
+++ muse-api/app/controllers/auth_controller.rb
@@ -0,0 +1,25 @@
+class AuthController < ApplicationController
+  def signup
+    user = User.new(signup_params)
+    if user.save
+      render json: { id: user.id, email: user.email, name: user.name }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      render json: { id: user.id, email: user.email, name: user.name }, status: :ok
+    else
+      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+    end
+  end
+
+  private
+  def signup_params
+    params.permit(:email, :password, :name)
+  end
+end
