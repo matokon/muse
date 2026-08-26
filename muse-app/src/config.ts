bfac8641c59ec128aff2b3f90786c506788458c4
Mateusz Okon <mateusz.okon04@gmail.com>
2026-08-26 18:44:00 +0200

    fix: switching to 54 version to be able to use expo app

--- muse-app/src/config.ts
+++ muse-app/src/config.ts
@@ -1,1 +1,10 @@
-export const API_URL = 'http://192.168.0.58:3000';
+import Constants from 'expo-constants';
+
+const API_PORT = 3000;
+
+function devApiUrl() {
+  const host = Constants.expoConfig?.hostUri?.split(':')[0];
+  return host ? `http://${host}:${API_PORT}` : `http://localhost:${API_PORT}`;
+}
+
+export const API_URL = devApiUrl();
